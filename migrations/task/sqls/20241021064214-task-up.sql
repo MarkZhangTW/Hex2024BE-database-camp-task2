b81373e76aedbab715174a80bbff11c6c56e6f57
Mark Zhang <markzhang1994@gmail.com>
2024-12-08 19:07:33 +0800

    Task5

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -196,25 +196,102 @@ WHERE u.email = 'lee2000@hexschooltest.io';
         -- 1. 預約人設為 `好野人`
         -- 2. 預約時間`booking_at` 設為2024-11-24 16:00:00
         -- 3. 狀態`status` 設定為即將授課
+INSERT INTO "COURSE_BOOKING" (
+    user_id,
+    course_id,
+    booking_at,
+    status
+)
+SELECT
+    u.id,
+    course.id,
+    '2024-11-24 16:00:00',
+    '即將授課'
+FROM "USER" u
+INNER JOIN "USER" coach ON coach."role" = 'COACH' AND coach.email = 'lee2000@hexschooltest.io'
+INNER JOIN "COURSE" course ON course.user_id = coach.id
+WHERE u.email = 'wXlTq@hexschooltest.io';
+
+INSERT INTO "COURSE_BOOKING" (
+    user_id,
+    course_id,
+    booking_at,
+    status
+)
+SELECT
+    u.id,
+    course.id,
+    '2024-11-24 16:00:00',
+    '即將授課'
+FROM "USER" u
+INNER JOIN "USER" coach ON coach."role" = 'COACH' AND coach.email = 'lee2000@hexschooltest.io'
+INNER JOIN "COURSE" course ON course.user_id = coach.id
+WHERE u.email = 'richman@hexschooltest.io';
 
 -- 5-2. 修改：`王小明`取消預約 `李燕容` 的課程，請在`COURSE_BOOKING`更新該筆預約資料：
     -- 1. 取消預約時間`cancelled_at` 設為2024-11-24 17:00:00
     -- 2. 狀態`status` 設定為課程已取消
+UPDATE "COURSE_BOOKING" cb
+SET
+    cancelled_at = '2024-11-24 17:00:00',
+    status = '課程已取消'
+FROM "USER" u
+WHERE cb.user_id = u.id AND u.email = 'wXlTq@hexschooltest.io';
 
 -- 5-3. 新增：`王小明`再次預約 `李燕容`   的課程，請在`COURSE_BOOKING`新增一筆資料：
     -- 1. 預約人設為`王小明`
     -- 2. 預約時間`booking_at` 設為2024-11-24 17:10:25
     -- 3. 狀態`status` 設定為即將授課
+INSERT INTO "COURSE_BOOKING" (
+    user_id,
+    course_id,
+    booking_at,
+    status
+)
+SELECT
+    u.id,
+    course.id,
+    '2024-11-24 17:10:25',
+    '即將授課'
+FROM "USER" u
+INNER JOIN "USER" coach ON coach."role" = 'COACH' AND coach.email = 'lee2000@hexschooltest.io'
+INNER JOIN "COURSE" course ON course.user_id = coach.id
+WHERE u.email = 'wXlTq@hexschooltest.io';
 
 -- 5-4. 查詢：取得王小明所有的預約紀錄，包含取消預約的紀錄
+SELECT
+    u.name          AS "預約會員",
+    c."name"        AS "預約課程",
+    cb.booking_at   AS "預約時間",
+    cb.status       AS "預約狀態",
+    cb.cancelled_at AS "取消預約時間"
+FROM "USER" u
+INNER JOIN "COURSE_BOOKING" cb ON cb.user_id = u.id
+INNER JOIN "COURSE" c ON c.id  = cb.course_id
+WHERE u.email = 'wXlTq@hexschooltest.io';
 
 -- 5-5. 修改：`王小明` 現在已經加入直播室了，請在`COURSE_BOOKING`更新該筆預約資料（請注意，不要更新到已經取消的紀錄）：
     -- 1. 請在該筆預約記錄他的加入直播室時間 `join_at` 設為2024-11-25 14:01:59
     -- 2. 狀態`status` 設定為上課中
+UPDATE "COURSE_BOOKING" cb
+SET
+    join_at = '2024-11-25 14:01:59',
+    status = '上課中'
+FROM "USER" u
+WHERE cb.user_id = u.id AND cancelled_at IS NULL AND u.email = 'wXlTq@hexschooltest.io';
 
 -- 5-6. 查詢：計算用戶王小明的購買堂數，顯示須包含以下欄位： user_id , total。 (需使用到 SUM 函式與 Group By)
+SELECT u.id AS user_id, u.name, SUM(cp.purchased_credits) AS total
+FROM "USER" u
+INNER JOIN "CREDIT_PURCHASE" cp ON cp.user_id = u.id
+WHERE u.email = 'wXlTq@hexschooltest.io'
+GROUP BY u.id;
 
 -- 5-7. 查詢：計算用戶王小明的已使用堂數，顯示須包含以下欄位： user_id , total。 (需使用到 Count 函式與 Group By)
+SELECT u.id AS user_id, u.name, COUNT(cb.id) AS total FROM "USER" u
+INNER JOIN "COURSE_BOOKING" cb ON cb.user_id = u.id AND join_at IS NOT NULL
+WHERE u.email = 'wXlTq@hexschooltest.io'
+GROUP BY u.id;
 
 -- 5-8. [挑戰題] 查詢：請在一次查詢中，計算用戶王小明的剩餘可用堂數，顯示須包含以下欄位： user_id , remaining_credit
     -- 提示：
@@ -222,6 +299,19 @@ WHERE u.email = 'lee2000@hexschooltest.io';
     -- from ( 用戶王小明的購買堂數 ) as "CREDIT_PURCHASE"
     -- inner join ( 用戶王小明的已使用堂數) as "COURSE_BOOKING"
     -- on "COURSE_BOOKING".user_id = "CREDIT_PURCHASE".user_id;
+SELECT u.id AS user_id, u.name, cp.total_credit - cb.used_credit AS remaining_credit
+FROM "USER" u
+INNER JOIN (
+    SELECT user_id, SUM(purchased_credits) AS total_credit
+    FROM "CREDIT_PURCHASE"
+    GROUP BY user_id
+) AS cp ON cp.user_id = u.id
+LEFT JOIN (
+    SELECT user_id, COUNT(id) AS used_credit
+    FROM "COURSE_BOOKING" WHERE join_at IS NOT NULL
+    GROUP BY user_id
+) AS cb ON cb.user_id = u.id
+WHERE u.email = 'wXlTq@hexschooltest.io';
 
 
 -- ████████  █████   █     ███  
